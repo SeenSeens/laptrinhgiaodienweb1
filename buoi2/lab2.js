60a6059feab3d91f966ca1d2891615d787aa4ea1
SeenSeens <truongtuan829@gmail.com>
2023-08-25 19:13:02 +0700

    buoi2final

--- buoi2/lab2.js
+++ buoi2/lab2.js
@@ -1,71 +1,197 @@
 angular.module( "myApp", [] ).controller( "myCtrl", ($scope) => {
-	$scope.A = 0;
-	$scope.B = 0;
-	$scope.C = 0;
+	// $scope.A = 0;
+	// $scope.B = 0;
+	// $scope.C = 0;
 	$scope.findMax = () => {
-		var max = $scope.A;
-		if($scope.B > max)
-			max = $scope.B;
-		if($scope.C > max)
-			max = $scope.C;
-		return max = 'MAX ' + max;
-	}
+		let a = $scope.A, b = $scope.B, c = $scope.C;
+		var max = a;
+		if(b > max)
+			max = b;
+		if(c > max)
+			max = c;
+		$scope.max = 'MAX ' + max;
+	},
+
 	// Bài 2
 	$scope.sort = () => {
-		var temp;
-		if($scope.A > $scope.B) { 
-			temp = $scope.A;
-			$scope.A = $scope.B;
-			$scope.B = temp;
+		let a = $scope.A, b = $scope.B, c = $scope.C, temp;
+
+		if(a > b) { 
+			temp = a;
+			a = b;
+			b = temp;
 		}
-		if($scope.A > $scope.C) {
-			temp = $scope.A;
-			$scope.A = $scope.C;
-			$scope.C = temp;
+		if(a > c) {
+			temp = a;
+			a = c;
+			c = temp;
 		}
-		if($scope.B > $scope.C) {
-			temp = $scope.B;
-			$scope.B = $scope.C;
-			$scope.C = temp;
+		if(b > c) {
+			temp = b;
+			b = c;
+			c = temp;
 		}
-		return $scope.A + ' ' + $scope.B + ' ' + $scope.C
-	}
+		$scope.sorts = a + ' ' + b + ' ' + c;
+	},
+
 	// Bài 3
 	$scope.giaiPTB1 = () => {
-		if($scope.A === 0) {
-			if($scope.B === 0)
-				return "Phương trình vô số nghiệm";
+		let a = $scope.A, b = $scope.B, c = $scope.C;
+		if(a === 0) {
+			if(b === 0)
+				$scope.result = "Phương trình vô số nghiệm";
 			else
-				return "Phương trình vô nghiệm";
+				$scope.result = "Phương trình vô nghiệm";
 		} else {
-			return -$scope.B / $scope.A;
+			$scope.result = b / a;
 		}
-	}
+	},
+
 	// Bài 4
 	$scope.giaiPTB2 = () => {
-		if($scope.A !== 0) {
-			let delta = Math.pow($scope.B, 2) - 4 * $scope.A *$scope.C;
+		let a = $scope.A, b = $scope.B, c = $scope.C;
+		if(a !== 0) {
+			let delta = Math.pow(b, 2) - 4 * a * c;
 			if(delta === 0) {
-				return "Phuong trinh co nghiem kep" + ( -$scope.B / ( 2 * $scope.A) );
+				$scope.result  = "Phương trình có nghiệm kép " + ( -b / ( 2 * a) );
 			} else {
 				if(delta < 0) {
-					return "Phuong trinh vo nghiem";
+					$scope.result  = "Phương trình vô nghiệm";
 				} else {
-					return ( -$scope.B + Math.sqrt(delta) ) / ( 2 * $scope.A );
-					return ( -$scope.B - Math.sqrt(delta) ) / ( 2 * $scope.A );
+					var x1 = ( -b + Math.sqrt(delta) ) / ( 2 * a );
+					var x2 = ( -b - Math.sqrt(delta) ) / ( 2 * a );
+					$scope.result = "Phuong trinh có 2 nghiệm phân biệt x1 = " + x1 + ", x2 = " + x2;
 				}
 			}
 		} else {
 			// Phuong trinh bac 1
-			if($scope.B === 0) {
-				if($scope.C === 0)
-					return "Phương trình vô số nghiệm";
+			if(b === 0) {
+				if(cC === 0)
+					$scope.result = "Phương trình vô số nghiệm";
 				else
-					return "Phương trình vô nghiệm";
+					$scope.result = "Phương trình vô nghiệm";
 			}
 			else {
-				return -$scope.C / $scope.B;
+				$scope.result = -c / b;
 			}
 		}
-	}
+	},
+
+	// Bài 5
+	$scope.equationType = "linear";
+	$scope.isVisible = true;
+	$scope.toggle = () => {
+        $scope.isVisible = $scope.equationType === "quadratic";
+    },
+
+    // Bài 6
+    $scope.cuoctaxi = () => {
+    	let a = $scope.A, b = $scope.B, c = $scope.C, km = $scope.km;
+
+    	if( km === 1 ) {
+    		$scope.result = "Số tiền bạn phải trả cho " + km + " km là " + km * 13000;
+    	}else {
+    		if( km >=2 && km < 30 ) {
+    			$scope.result = "Số tiền bạn phải trả cho " + km + " km là " + km * 12000;
+    		} else {
+    			$scope.result = "Số tiền bạn phải trả cho " + km + " km là " + km * 11000;
+    		}
+    	}
+    },
+
+    // Bài 7
+    $scope.kttamgiac = () => {
+    	let a = $scope.A, b = $scope.B, c = $scope.C;
+    	// Tam giác đều
+    	let tamgiacdeu = () => {
+    		if( ( a === b ) && ( b === c ) ) return true;
+    	}
+    	// Tam giác cân
+    	let tamgiaccan = () => {
+    		if( ( a === b ) || ( a === c ) || ( b === c )) return true;
+    	}
+    	// Tam giác vuông
+    	let tamgiacvuong = () => {
+    		if ( ( Math.pow(a, 2) === Math.pow(b, 2) + Math.pow(c, 2) ) || ( Math.pow(b, 2) === Math.pow(a, 2) + Math.pow(c, 2) ) || ( Math.pow(c, 2) === Math.pow(a, 2) + Math.pow(b, 2) ) ) return true;
+    	}
+    	// Kiểm tra điều kiện lập thành tam giác
+    	if( ( a + b > c ) && ( a + c > b ) && ( b + c > a ) || ( a > 0 ) && ( b > 0 ) && ( c > 0 ) ) {
+			// Tam giác đều
+			if( tamgiacdeu() )
+				$scope.result = "Tam giác đều";
+			// Tam giác cân
+			else if( tamgiaccan() )
+				$scope.result = "Tam giác cân";
+			// Tam giác vuông
+			else if ( tamgiacvuong() )
+				$scope.result = "Tam giác vuông";
+			// Tam giác vuông cân
+			else if ( tamgiaccan() && tamgiacdeu() )
+                $scope.result = "Tam giác vuông cân";
+            // Tam giác thường
+            else
+            	$scope.result = "Tam giác thường";
+
+		} else {
+			$scope.result = "Đây không phải là tam giác"
+		}
+    },
+
+    // Bài 8
+   	$scope.lietkesonguyento = () => {
+        let n = $scope.N;
+
+        // Kiểm tra số nguyên tố
+        let ktSNT = (x) => {
+            if (x < 2) return false;
+            for (let i = 2; i <= Math.sqrt(x); i++) {
+                if (x % i === 0) return false;
+            }
+            return true;
+        }
+
+        // Liệt kê các số nguyên tố
+        let lkSNT = (n) => {
+            const dsSNT = [];
+            for (let i = 2; dsSNT.length < n; i++) {
+                if (ktSNT(i)) dsSNT.push(i);
+            }
+            $scope.result = "Danh sách các số nguyên tố " + dsSNT.join(', ');
+        }
+        
+        lkSNT(n);
+    },
+
+    // Bài 9
+    $scope.findUCLNBCNN = () => {
+    	let a = $scope.A, b = $scope.B;
+    	// Ước chung lớn nhất
+    	let UCLN = (a, b) => {
+    		while ( a != b ) {
+		        if ( a > b )
+		            a = a - b;
+		        else
+		            b = b - a;
+		    }
+		    return a;
+    	}
+    	// Bội chung nhỏ nhất
+    	let BCNN = () => {
+    		return a * b / UCLN(a, b);
+    	}
+    	$scope.result = "Ước chung lớn nhất " + UCLN(a, b) + "\n" + "Bội chung nhỏ nhất " + BCNN(a, b);
+
+    },
+
+    // Bài 10
+    $scope.demUocSo = () => {
+    	let n = $scope.N, dem = 0;
+    	let Dem_UocSo = (n) => {
+    		for(let i = 1; i <= n; i++) {
+    			if( n % i === 0) dem++;
+    		}
+    		return dem;
+    	}
+    	$scope.result = Dem_UocSo(n);
+    }
 } );
